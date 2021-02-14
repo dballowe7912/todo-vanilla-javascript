74423add1f38b5a07978fe8878f17e310bb08c53
Donald Ballowe <dballowe7912@gmail.com>
2021-02-13 23:47:52 -0600

    finished clear all functionality

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-document.querySelector('.addTodoForm').addEventListener('submit', handleSubmit);
+// Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
 document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click', handleClearAll);
 
-function handleSubmit(event) {
+// Event Handlers
+function handleSubmitForm(event){
     event.preventDefault();
-    let input = document.querySelector('.addTodoInput');
+    let input = document.querySelector('input');
     if (input !== '') {
         addTodo(input.value)
+    } else {
+        return;
     }
     input.value = '';
 }
 
-const addTodo = (todo) => {
-    let ul = document.querySelector('ul');
-    let li = document.createElement('li');
-    li.innerHTML = `
-    <span class='todo-item'>${todo}</span>
-    <button name="checkButton"><i class="fas fa-check-square"></i></button>
-    <button name="deleteButton" ><i class="fas fa-trash"></i></button>
-    `;
-    li.classList.add('todo-list-item');
-    ul.appendChild(li);
+function addTodo(todo) {
+   const ul = document.querySelector('ul');
+   const li = document.createElement('li');
+   li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <button name="deleteButton" ><i class="fas fa-trash"></i></button>'
+   `;
+   li.classList.add('todo-list-item');
+   ul.appendChild(li);
 }
 
 function handleClickDeleteOrCheck(event) {
-    if (event.target.name === 'checkButton') {
+    if (event.target.name === 'checkButton'){
         checkTodo(event);
-    } else if (event.target.name === 'deleteButton') {
+    }
+    if (event.target.name === 'deleteButton'){
         deleteTodo(event);
     }
 }
 
-function checkTodo(event) {
-    console.log('clicked')
-    let item = event.target.parentNode;
-    if (item.style.textDecoration == 'line-through') {
-        item.style.textDecoration == 'none';
-    } else item.style.textDecoration = 'line-through';
+function checkTodo(e) {
+    let item = e.target.parentNode;
+    if (item.style.textDecoration == 'line-through')
+        item.style.textDecoration = 'none';
+    else
+        item.style.textDecoration = 'line-through';
 }
 
 function deleteTodo(e) {
@@ -46,4 +52,8 @@ function deleteTodo(e) {
     });
 
     item.classList.add('todo-list-item-fall');
+}
+
+function handleClearAll(event) {
+    document.querySelector('ul').innerHTML = '';
 }
